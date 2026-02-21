1aeae5ba6f190d06600ccf1e7b0c342a8ec2a088
SILAS-JOB <tiilas777@gmail.com>
2026-02-21 19:23:15 -0300

    feat: simple connect fn

--- auth_api/src/db/connect.rs
+++ auth_api/src/db/connect.rs
@@ -1,3 +1,12 @@
-use tokio_postgres::Client;
+use tokio_postgres::{Client, Error, NoTls};
 
-pub fn connect_db() {}
+pub async fn connect_db() -> Result<(), Error> {
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres", NoTls).await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Error Connecting to database: {}", e);
+        }
+    });
+    Ok(())
+}
